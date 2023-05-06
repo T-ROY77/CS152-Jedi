86c49db83b299cadabe76b2fc269d4e8f6ef0b74
Troy <troy,perez@sjsu.edu>
2023-05-06 13:21:28 -0700

    done

--- src/main/scala/expression/FunCall.scala
+++ src/main/scala/expression/FunCall.scala
@@ -4,20 +4,7 @@ import context._
 import expression._
 import value._
 
-//****************
-// execute code not working
-
-
 case class FunCall(operator: Identifier, operands: List[Expression]) extends Expression:
-
-//  def execute(env: Environment) =
-//    val arguments = operands.map(_.execute(env))
-//    val maybeClosure = operator.execute(env)
-//    maybeClosure match {
-//      case closure: Closure => closure.apply(arguments)
-//      case _ => alu.execute(operator, arguments)
-//    }
-
 
     def execute(env: Environment) =
       val arguments = operands.map(_.execute(env))
